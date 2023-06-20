a915e7d6db72f480338ab1754f10e8c40e5f1aeb
duong <duongabc100@gmail.com>
2023-06-20 20:26:36 +0700

    re-calc screen size for iframe ARS-event game

--- src/pages/ARShooterEventGame/index.js
+++ src/pages/ARShooterEventGame/index.js
@@ -1,30 +1,51 @@
-import React, {useRef, useEffect ,useState} from 'react';
+import React, { useLayoutEffect, useRef } from "react";
 
 const ARShooterEventGame = () => {
   const iframeRef = useRef(null);
   const getParamFromUrl = (url) => {
     if (url.indexOf("?") > -1) {
       let parts = url.split("?");
-      return "?"+ parts[1]
-     
-      } else {
-        return "";
-      }
+      return "?" + parts[1];
+    } else {
+      return "";
+    }
   };
-  const param =  getParamFromUrl(window.location.href);
+  const param = getParamFromUrl(window.location.href);
   const _src = "https://rlgl2.brandgames.vn/ARShooterEvent/index.html" + param;
+  const updateIframeHeight = () => {
+    const iframe = iframeRef.current;
+    if (iframe) {
+      const viewportHeight = window.innerHeight;
+      const iframeHeight =
+        viewportHeight -
+        (window.innerHeight - document.documentElement.clientHeight);
+      iframe.style.height = `${iframeHeight}px`;
+    }
+  };
+
+  useLayoutEffect(() => {
+    updateIframeHeight();
+    window.addEventListener("resize", updateIframeHeight);
+
+    return () => {
+      window.removeEventListener("resize", updateIframeHeight);
+    };
+  }, []);
+  const urlBarHeight = 50;
   return (
-    <div>
+    <>
       <iframe
         ref={iframeRef}
         src={_src}
-        style={{ width: '100%', height: '100vh', border: 'none' }}
-        allow="camera;accelerometer;gyroscope;magnetometer;" > 
-      </iframe>
-     
-    </div>
+        style={{
+          width: "100%",
+          height: `calc(100vh - ${urlBarHeight}px)`,
+          border: "none",
+        }}
+        allow="camera;accelerometer;gyroscope;magnetometer;"
+      ></iframe>
+    </>
   );
- 
 };
 
 export default ARShooterEventGame;
